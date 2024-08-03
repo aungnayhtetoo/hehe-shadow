74d503af4bef637cdb6cb828e18a181eb77fcae4
Aung Nay Htet Oo <aungnayhtetoo37@gmail.com>
2024-08-03 12:37:49 -0500

    base main

---
+++ components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, SafeAreaView, View } from 'react-native'
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Button title='Press'/>
+        </View>
+    </SafeAreaView>
+  )
+}
