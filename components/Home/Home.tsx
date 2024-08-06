c97980c21f0ee1974cd3fc643c0cd76c070e3dd7
Aung Nay Htet Oo <aungnayhtetoo37@gmail.com>
2024-08-05 19:45:10 -0500

    fix prettier

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Button, SafeAreaView, View } from 'react-native'
+import React from 'react';
+import { Button, SafeAreaView, View } from 'react-native';
 
 export const Home = () => {
   return (
     <SafeAreaView>
-        <View>
-            <Button title='Press'/>
-        </View>
+      <View>
+        <Button title="Press" />
+        <Button title="Press" />
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
